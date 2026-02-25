18056f4ae560a2509f3efc445012fd25472c55c1
Samaran <samaran270s@gmail.com>
2026-02-25 21:09:19 +0530

    Refactor twoSum method for clarity and efficiency
    Refactor twoSum method to remove redundant checks and improve readability.

--- two_sum.py
+++ two_sum.py
@@ -15,41 +15,15 @@ class Solution(object):
             jindex = index + 1
             if index < len(nums)-1:
                 for j in nums[jindex:]:
-                    if index==jindex:
-                        break  
-                    if i+j == target:
+                    if i+j == target and index != jindex:
                         l = [index,jindex]
                         return l
-
+                    jindex = jindex +1 
                     
-            index = index + 1
-
-i = Solution()
-i.twoSum([1,2,3],3)class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-
-        index = 0
-        jindex = 0
-        for i in nums:
-            
-            #[1,2,3,4] --> so no repatition
-            #
-            jindex = index + 1
-            if index < len(nums)-1:
-                for j in nums[jindex:]:
-                    if index==jindex:
-                        break  
-                    if i+j == target:
-                        l = [index,jindex]
-                        return l
 
                     
             index = index + 1
 
 i = Solution()
 i.twoSum([1,2,3],3)
+                
