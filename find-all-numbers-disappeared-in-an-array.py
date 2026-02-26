a07ca6b966fad80129d7a1a8389c43e7d0ac3dbb
Samaran <samaran270s@gmail.com>
2026-02-26 23:19:59 +0530

    Refactor logic to find missing numbers using dictionaries
    Refactor the function to use dictionaries for tracking indices and present numbers, simplifying the logic for finding missing numbers.

--- find-all-numbers-disappeared-in-an-array.py
+++ find-all-numbers-disappeared-in-an-array.py
@@ -5,29 +5,22 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        # input_index = [0,1,2,.. n-1]
-        # so for each array, the range is 1 to len(Array)
-            # these are the values that can be inside the arr
-        
-        #store the index in list.
-        # check the index_list with the input list.
-            #if any number missed, add to new list
-
-        #excluding 0 from the index list.
-
-
-        index_list = []
+        index_dict = {}  
+        nums_dict = {}  
         op = []
-        i = 0
 
-        for index in range(1,len(nums)+1):
-            index_list.append(index)
+       
+        for index in range(1, len(nums)+1):
+            index_dict[index] = True
+
+        # Store numbers present in nums
+        for number in nums:
+            nums_dict[number] = True
+
         
+        for num in index_dict:
+            if num not in nums_dict:
+                op.append(num)
 
-
-        for j in nums:
-            if j not in index_list:
-                op.append(j)
-                
-        print(j)
+        print(op)
         return op
