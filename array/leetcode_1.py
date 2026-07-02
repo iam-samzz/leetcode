fdab5446c15bb3dd8d11776d57440c7ee6f0941e
iam-samzz <samaran270s@gmail.com>
2026-07-03 01:42:27 +0530

    1. Two Sum

---
+++ array/leetcode_1.py
@@ -0,0 +1,12 @@
+class Solution:
+   def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        result =[]
+        for i in range(0,len(nums)):
+            for j in range(0,len(nums)):
+                
+                if j!=i:
+                    if (nums[i] + nums[j] == target):
+                        result.append(i)
+                        result.append(j)
+                        return result
