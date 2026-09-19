caee34f8390982f091c5d7c79ee5a55e59f0eb3b
iam-samzz <samaran270s@gmail.com>
2026-09-19 22:45:55 +0530

    kadane's algorithm

---
+++ array/leetcode_53.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxSubArray(self, nums: list[int]) -> int:
+        
+
+        n = len(nums)
+        current_sum = nums[0]
+        max_sum = nums[0]
+
+        for i in range(1,n):
+            current_sum += nums[i]
+            if current_sum < nums[i]:
+                current_sum = nums[i]
+            
+            max_sum = max(current_sum,max_sum)
+
+        return max_sum
