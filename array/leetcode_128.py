89b5a74e75d6e5954c513bd19fa0661631e2d44b
iam-samzz <samaran270s@gmail.com>
2026-09-22 22:23:08 +0530

    Longest Consecutive Sequence v2- O(n) time, and O(n)-space

--- array/leetcode_128.py
+++ array/leetcode_128.py
@@ -1,26 +1,17 @@
 class Solution:
     def longestConsecutive(self, nums: list[int]) -> int:
-        
-        if len(nums) <=0:
-            return 0
-        
-        nums.sort()
-        
-        
-        left = 1
-        c=1
-        max_l = 1
+        nums_set = set(nums)
 
-        while left < len(nums):
-            if nums[left] == nums[left-1]:
-                left += 1
-            elif nums[left] == nums[left-1]+1:
-                c += 1
-                max_l = max(max_l,c)
-                left += 1
-            else:
-                c = 1
-                max_l = max(max_l,c)
-                left +=1
+        max_l = 0
 
+        for element in nums_set:
+            if element-1 not in nums_set:
+                #its the starting of the sequence
+                z = 1
+                while (element+z) in nums_set:
+                    z += 1
+                max_l = max(max_l,z)
+        
         return max_l
+
+            
