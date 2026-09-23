e13d2601d907d57dc680904ed3a6e97962014478
iam-samzz <samaran270s@gmail.com>
2026-09-23 20:05:30 +0530

    spiral matrix -important -revise -medium

---
+++ array/leetcode_54.py
@@ -0,0 +1,34 @@
+class Solution:
+    def spiralOrder(self, matrix: list[list[int]]) -> list[int]:
+        
+
+        
+        c = len(matrix[0])        
+        r = len(matrix)
+
+        top = 0
+        left = 0
+
+        right = c
+        bottom = r
+        res = []
+        while top < bottom and left < right:
+
+            for i in range(left,right):
+                res.append(matrix[top][i])
+            top += 1
+
+            for i in range(top,bottom):
+                res.append(matrix[i][right-1])
+            right -= 1
+
+            if top < bottom:
+                for i in range(right-1,left-1,-1):
+                    res.append(matrix[bottom-1][i])
+                bottom -= 1
+
+            if left < right:
+                for i in range(bottom-1,top-1,-1):
+                    res.append(matrix[i][left])
+                left += 1
+        return res
